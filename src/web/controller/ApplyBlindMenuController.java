51302e5eb3fd6ff324a2833f0796709022069e44
ats3059@naver.com <ats3059@naver.com>
2020-06-19 14:06:28 +0900

    ㅎㅇㅎㅇ

--- src/web/controller/ApplyBlindMenuController.java
+++ src/web/controller/ApplyBlindMenuController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/fran/apply/blind")
 public class ApplyBlindMenuController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+//	ㅎㅇㅎㅇ
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.getRequestDispatcher("/WEB-INF/views/fran_board/applyBlind.jsp").forward(req, resp);
