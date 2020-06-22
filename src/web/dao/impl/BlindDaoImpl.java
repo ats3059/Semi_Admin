25fa690276050e5cf48791ff534e5218b0b56c44
ats3059@naver.com <ats3059@naver.com>
2020-06-22 19:35:29 +0900

    2차 변경

--- src/web/dao/impl/BlindDaoImpl.java
+++
@@ -1,7 +0,0 @@
-package web.dao.impl;
-
-import web.dao.face.BlindDao;
-
-public class BlindDaoImpl implements BlindDao {
-
-}
--- src/web/dao/impl/MenuDaoImpl.java
+++
@@ -1,7 +0,0 @@
-package web.dao.impl;
-
-import web.dao.face.MenuDao;
-
-public class MenuDaoImpl implements MenuDao{
-
-}
